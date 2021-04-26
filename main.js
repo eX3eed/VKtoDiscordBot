af65be248deab4ec3e38ea74c29d51356ce8389d
Danil <capitanaxe1337@gmail.com>
2021-04-27 00:29:27 +0300

    i think that is working, but i have any troubles to send a message to discord and need to create file called ('config.json')

--- main.js
+++ main.js
@@ -3,15 +3,16 @@ const client = new Discord.Client();
 const {VKApi, ConsoleLogger, BotsLongPollUpdatesProvider} = require('node-vk-sdk');
 const fs = require('fs');
 const filename = 'vkdata.json'
+const token = 'MzIxNjc4NDc1NTExNDYzOTM4.WTbQbQ.IjDysJvuAqq64g-SFC6wRktb0g4'
 
-let photosURLs = '';
+let photosURLs = [];
 let message = '';
 
 function updateFile() {
-    fs.writeFile(filename, " ", (err) => {
+    fs.writeFileSync(filename, " ", (err) => {
       if (err) throw err;
-      console.log("Создал файл");
     })
+    console.log("Почистил файл");
 }
 
 // Вк обвязка + запись в файл (в случае если данные уже существуют в файле, нужно сделать перезапить этих файлов)
@@ -40,14 +41,12 @@ setInterval(() => {
   let data = JSON.parse(raWdata);
 
 //Проверяем, указаны ли фото в посте, если нет то записываем только текст
-  function checkImageFromData(objectData) {
-    if(objectData == false){
+  function checkImageFromData() {
       setTimeout(() => {
         updateFile();
-      }, 3000);
+      }, 10000);
         console.log("Картинки не было")
         console.log(message);
-    }
   }
 
 //В случае, если картинки есть, мы вытаскиваем данные из json объектов с high res качеством (которое дает вк)
@@ -55,64 +54,66 @@ setInterval(() => {
     for (let i = 0; i < photosArray.length; i++) {
       let getURLArray = photosArray[i].photo.sizes;
       for (let k = 0; k < getURLArray.length; k++) {
-        if (k == getURLArray.length - 1) {
-          photosURLs += getURLArray[k].url + " ";
+        if (k === getURLArray.length - 1) {
+          photosURLs.push(getURLArray[k].url)
         }
       }
     }
   }
 
 //Проверяем, есть ли данные, в случае если данных нет, мы очищаем файл
-  if (data[0] == undefined){ 
-    setTimeout(() => {
+  if (data[0] === undefined){
       updateFile();
-    }, 3000);
       console.log("данных не было")
   }
   else
   {
     message = data[0].object.text;
     let temp = 'attachment' in data[0].object
-    checkImageFromData(temp);
+    if(temp === false){
+      checkImageFromData();
+    }
     let photosArray = data[0].object.attachments;
-    if(photosArray == undefined){
+    if(photosArray === undefined){
       setTimeout(() => {
         updateFile();
-      }, 3000);
-      console.log(message)
+      }, 10000);
     }
     else{
         getHighResImages(photosArray)
+        console.log(photosURLs)
     }
   }
     
     console.log("записал данные, все ок")
     updateFile();
 
-    console.log(photosURLs);
-    console.log(message)
-
-    message = undefined;
-    photosURLs = undefined;
-}, 26000);
+}, 28000);
 
 //Сделать проверку, в случае если нет текста в посте, но есть картинки
 //Проверить код на работоспособность
 //Подключить дискорд уже наконец-то, заебался ебаться с правильным получением данных
 
+client.login(token);
 //Дискорд обвязка
-// client.on('ready', () => {
-//     console.log('Logged in ass ${client.user.tag}!');
-// })
+client.on('ready', () => {
+    console.log('Discord Bot activated');
+    let temp = client.channels.cache.find(channel => channel.id === '832692528095559753')
 
-// //client.channels.get('832692528095559753').send(formatingText);
-
-// client.on("message", (msg) => {
-//     if(msg.content === 'hello'){
-//         const attachemntPhoto = new Discord.MessageAttachment(photosURLs.split(' '));
-//         msg.channel.send(message,attachemntPhoto);
-//     }
-// });
+    setInterval( () => {
+        if( photosURLs !== undefined && message !== undefined){
+            temp.send(message);
+            for (let i = 0; i < photosURLs.length; i++) {
+                const attachemntPhoto = new Discord.MessageAttachment(photosURLs[i]);
+                temp.send(attachemntPhoto)
+            }
+            photosURLs = undefined;
+            message = undefined;
+        }
+        else{
+            console.log('Данных не было')
+        }
+    }, 40000)
+})
 
 // //По хорошему, стоит бы вынести токен дискорда в отдельный файл
-// client.login('MzIxNjc4NDc1NTExNDYzOTM4.WTbQbQ._fuQ_ZlbddOeKU_6X1Er-bGDJM8')
